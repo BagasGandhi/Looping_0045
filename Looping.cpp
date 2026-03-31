366488c75dfce58bf03e1abf16309ba1a4bc0b9b
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-31 18:13:54 +0800

    Fix comment to accurately reflect loop iteration count in FOR loop

--- Looping.cpp
+++ Looping.cpp
@@ -6,9 +6,21 @@ int main(){
     int x;
 
     //Perulangan dengan FOR
-    //mencetak tulisan sebanyak 5 kali
-    cout <<"PERULANGAN FOR"<<endl;
+    //mencetak tulisan sebanyak 4 kali
+    cout<<"PERULANGAN FOR"<<endl;
     for(int i=0; i<4; i++){
         cout <<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+    //untuk mendapatkan urutan angka acak yang berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
     }
 }
